8ed27dacf45582b673d169c1e150711a7951b277
42fat <91491016+42fat@users.noreply.github.com>
2022-01-29 20:50:57 +0300

    Add files via upload

--- roadmap-go/Go/step6/panicADNrecover.go
+++ roadmap-go/Go/step6/panicADNrecover.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+func main() {
+	defer func() {
+		r := recover()
+		fmt.Println(r)
+	}()
+	a := []string{"a", "b"}
+	myprint(a, 0)
+	panic("break")
+	fmt.Println("Normal exit")
+}
+
+func myprint(arr []string, index int) {
+	if index < 0 || index > len(arr) {
+		panic("Out of bound")
+	}
+	fmt.Println(arr[index])
+}
+
+func print(arr []string, index int) {
+	fmt.Println(arr[index])
+}
